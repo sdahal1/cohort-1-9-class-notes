93997618928147d3139bfa25a50a8db1ddfbf7d9
sdahal1 <raabdahl@gmail.com>
2023-01-09 12:07:18 -0800

    first day

---
+++ 2.7-debugger/2.7-example.js
@@ -0,0 +1,23 @@
+let greatestPlayerOfAllTime = {
+    name: "Lebron James",
+    team: "Lakers",
+    birthplace: "Akron, Ohio",
+    totalPoints: 35000,
+    doesClassAgree: false,
+    previousTeams: ["Cleveland", "Miami"],
+    address: {
+        street: "1110 hollywood blvd",
+        city: "Los Angeles",
+        state: "California"
+    }
+}
+
+
+
+let warmestCities = ["Miami", "Tampa", "San Diego", "Los Angeles", "Austin"]; 
+//index #:            0         1           2           3            4  
+
+for(let i = 0; i<warmestCities.length; i++){
+    console.log(i)
+    console.log(warmestCities[i])
+}

34e5418bb194d6dcaa212ec0d7f899ff1e42968f
sdahal1 <raabdahl@gmail.com>
2023-01-16 12:56:28 -0800

    w1d5pm

---
+++ 3.6-modules/fileOne.js
@@ -0,0 +1,8 @@
+const {players, teams} = require("./fileTwo")
+
+
+
+players.forEach(player=>{
+    console.log("player is: ", player.name)
+})
+
---
+++ 3.6-modules/fileTwo.js
@@ -0,0 +1,12 @@
+const players = [
+    {name: "Lebron", ppg: 30.2},
+    {name: "Durant", ppg: 33.2},
+    {name: "Kobe", ppg: 32.2},
+]
+
+const teams = [
+    {name: "Lakers", city: "Los Angeles"},
+    {name: "Celtics", city: "Boston"},
+]
+
+module.exports = {players,teams}

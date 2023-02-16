3ebe523eabe1f8f369c4455902c49efff8425ccc
sdahal1 <raabdahl@gmail.com>
2023-02-16 08:01:13 -0800

    w5d4am

---
+++ 20.3-selecting-elements-on-the-dom/starter-national-parks/index.js
@@ -0,0 +1,42 @@
+const h1 = document.querySelector("h1") //selects the first h1 element in the page
+
+
+// setTimeout(() => {
+//     h1.innerText = "Huh who dis"
+    
+// }, 2000);
+
+
+// setInterval(()=>{
+//     //code to run goes here
+//     h1.innerText += "!"
+// }, 1000)
+
+console.log(h1);
+// h1.remove();
+
+const section1 = document.querySelector("section") //grabs the first section element
+
+const allSections = document.querySelectorAll("section")
+// allSections.forEach(section=>{
+//     console.log(section);
+// })
+const allh2 = document.querySelectorAll("h2");
+// console.log(allh2)
+
+const allDescriptionDivs =  document.querySelectorAll(".description-display");
+
+// console.log(allDescriptionDivs)
+
+const starButtons = document.querySelectorAll(".rate-button");
+// console.log(starButtons);
+
+const allAreas = document.querySelectorAll(".area-display .value");
+console.log(allAreas)
+
+const byePTag = document.querySelector("#footer #b")
+console.log(byePTag)
+
+
+const GrandcanyonArea = document.querySelector("#GC .area-display .value");
+console.log(GrandcanyonArea);

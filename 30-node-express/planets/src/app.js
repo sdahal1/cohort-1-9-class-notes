64d11d6cc20f011d3c03495649bbb7e490acd0d2
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-03-13 11:23:14 -0700

    add query and route params, and error handling

--- 30-node-express/planets/src/app.js
+++ 30-node-express/planets/src/app.js
@@ -31,8 +31,47 @@ app.get('/', (req, res, next) => {
 
 app.get('/current-time', (req, res, next) => {
   let now = new Date();
-  res.send(now.toString());
+  // toSting, misspelled, so it will go to error handling
+  res.send(now.toSting());
 })
+
+app.get('/greeting', (req, res, next) => {
+  // let name = req.query.name;
+  let { name, planet = 'Earth' } = req.query;
+  // if they didn't include a name, handle the error
+  if (!name) {
+    next('Did not include a name in the greeting route');
+  } else {
+    res.send(`greetings from planet ${planet}, ${name}!`);
+  }
+})
+
+
+app.get('/planets/coolestPlanet', (req, res, next) => {
+  res.send('The coolest planet is the one that you are on :D')
+})
+
+let closerPlanets = ['Mercury', 'Venus'];
+let fartherPlanets = ['Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+app.get('/planets/:planet', (req, res, next) => {
+  let { planet } = req.params;
+  if (closerPlanets.includes(planet)) {
+    res.send(`The planet ${planet} is closer to the Sun than Earth.`)
+  } else if (fartherPlanets.includes(planet)) {
+    res.send(`The planet ${planet} is farther from the Sun than Earth`)
+  } else {
+    res.send(`I don't know about ${planet}.`);
+  }
+})
+
+
+// error handling
+// you can tell from the 4 params
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.send('There was an error.')
+})
+
 
 // export the app
 module.exports = app;

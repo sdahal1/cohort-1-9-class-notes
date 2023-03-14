8f165fba5a4719106df3fdc5d005e265eba6fb19
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-03-14 07:47:27 -0700

    add planets data and route-level middleware

---
+++ 30-node-express/planets/data/index.js
@@ -0,0 +1,5 @@
+// credit to https://github.com/Lazzaro83/Solar-System/blob/master/planets.json
+
+const planets = require('./planets.json');
+
+module.exports = planets;
--- 30-node-express/planets/src/app.js
+++ 30-node-express/planets/src/app.js
@@ -6,12 +6,8 @@ const app = express();
 // do other useful things
 // add in middleware
 
-app.use((req, res, next) => {
-  // log the incoming request
-  console.log('there was a request');
-  // move on to the next piece of middleware or main task/handler function
-  next();
-})
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 // define routes
 
@@ -35,21 +31,32 @@ app.get('/current-time', (req, res, next) => {
   res.send(now.toSting());
 })
 
-app.get('/greeting', (req, res, next) => {
+function validateName(req, res, next) {
+  // if the name exists and is valid, just move to the next piece of middleware in the pipeline
+  if (req.query.name) {
+    next();
+  } else {
+    // if the name wasn't valid, go into error handling
+    next('Did not include a name');
+  }
+}
+
+// app-level middleware: runs on every request
+// bad because not every request should have a name
+// app.use(validateName);
+
+// route-level middleware: runs only for this route
+app.get('/greeting', validateName, (req, res, next) => {
   // let name = req.query.name;
   let { name, planet = 'Earth' } = req.query;
-  // if they didn't include a name, handle the error
-  if (!name) {
-    next('Did not include a name in the greeting route');
-  } else {
-    res.send(`greetings from planet ${planet}, ${name}!`);
-  }
-})
 
+  res.send(`greetings from planet ${planet}, ${name}!`);
+})
 
 app.get('/planets/coolestPlanet', (req, res, next) => {
   res.send('The coolest planet is the one that you are on :D')
 })
+
 
 let closerPlanets = ['Mercury', 'Venus'];
 let fartherPlanets = ['Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
@@ -61,6 +68,20 @@ app.get('/planets/:planet', (req, res, next) => {
     res.send(`The planet ${planet} is farther from the Sun than Earth`)
   } else {
     res.send(`I don't know about ${planet}.`);
+  }
+})
+
+// using planets data from data directory
+
+const planets = require('../data');
+app.get('/planets', (req, res, next) => {
+  // if limitedData is true, only send some of the info
+  if (req.query.limitedData && req.query.limitedData.toLowerCase() !== 'false') {
+    let limitedDataPlanets = planets.map(planet => ({ name: planet.name, image: planet.image, description: planet.description }));
+    res.send(limitedDataPlanets);
+  } else {
+    // otherwise, send everything
+    res.send(planets);
   }
 })
 

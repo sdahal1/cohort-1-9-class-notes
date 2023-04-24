9ffac6142ae32faf5030c32c876984c65d37d5b0
kelvin711 <arauzkelvin99@gmail.com>
2023-04-24 10:11:55 -0500

    linked list  conceptual talk

---
+++ 43-data-structures/linkedList/linkedList.js
@@ -0,0 +1,20 @@
+// object oriented programming
+// javascript uses prototypical inheritance
+
+// classical oop vs javascript - 
+
+class Node {
+    constructor(value, next = null) { //constructor; how to make our objects
+        //properties of our objects
+        this.value = value;
+        this.next = next;
+    }
+    //methods of our objects
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+}

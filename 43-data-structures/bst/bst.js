6002271445692bddfc53727a53be385d50bb01a1
kelvin711 <arauzkelvin99@gmail.com>
2023-04-26 13:49:46 -0500

    remove BST

--- 43-data-structures/bst/bst.js
+++ 43-data-structures/bst/bst.js
@@ -46,9 +46,84 @@ class BST {
             return null;
         }
     }
+
+    remove(key) {
+        if (this.key === key) {
+            // check to see if bst has two children
+            if ( this.left && this.right ) {
+                // choose to find the max on the left or the min on the right
+                let nodeToPromote = this.right._findMin();
+                // let nodeToPromote = this.right._findMax();
+                this.key = nodeToPromote.key;
+                this.value = nodeToPromote.key;
+                // remove the duplicate node from our bst
+                nodeToPromote.remove(nodeToPromote.key)
+            } else if ( this.left ) { // left child
+                this._replaceWith(this.left)
+            } else if ( this.right ) { // right child
+                this._replaceWith(this.right)
+            } else { // no children at all
+                this._replaceWith(null);
+            }
+        }
+
+        else if (key < this.key && this.left) { // should i go to the left and go to the left
+            this.left.remove(key); // going left 
+        } else if (key > this.key && this.right) { // should i go to the right and go to the right
+            this.right.remove(key); // going right
+        } else {
+            return null;
+        }
+    }
+
+    _replaceWith(node) {
+        // check if the node we are replacing as a parent
+        // if it does we can connect the refrences from the parent node to the replacement node
+        if ( this.parent ) { //ask if the node has a parent
+            if ( this.parent.left === this ) {
+                // current node to be replaced on the left side of the parent
+                this.parent.left = node;
+            } else if ( this.parent.right === this ) {
+                this.parent.right = node;
+            }
+            // replace the replacements nodes parent refrence
+            if ( node ) {
+                node.parent = this.parent
+            }
+        } else { // the node is a root node we just copy over the properties
+            if (node) {
+                this.key = node.key;
+                this.value = node.value;
+                this.left = node.left;
+                this.right = node.right;
+            } else { //user passsed in null
+                this.key = null;
+                this.value = null;
+                this.left = null;
+                this.right = null;
+            }
+        }
+    }
+
+    //find min helper
+    _findMin(node) {
+        if (!this.left) { // if i cant go right aka right is null
+            return this; //return me that node
+        }
+        return this.left._findMin(); // keep traversing right recursively
+    }
+
+    //find min helper
+    _findMax(node) {
+        if (!this.right) { // if i cant go left aka left is null
+            return this; //return me that node
+        }
+        return this.right._findMax(); // keep traversing left recursively
+    }
 }
 
-let BTS = new BST()
-BTS.insert(4,4).insert(2,2).insert(6,6)
+let BTS = new BST(5,5)
+BTS.insert(4,4).insert(2,2).insert(6,6).insert(7,7).insert(8,8).insert(5,5)
 
-console.log(BTS);
+BTS.remove(5)
+BTS.remove(8)
